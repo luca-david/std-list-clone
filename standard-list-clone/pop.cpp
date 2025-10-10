f4c85272627238cdae4f674206063b3635e5231e
Luca David <robertlucadavid@gmail.com>
2025-10-10 20:45:43 +0300

    implemented pop_back

---
+++ standard-list-clone/pop.cpp
@@ -0,0 +1,36 @@
+#include "pop.h"
+#include <iostream>
+
+int* pop_back(int* array, int* pSize)
+{
+	//make an array that is smaller by one element
+	int* smallerArray = new int[*pSize - 1];
+
+	//go through the array and replace every element with the one in front(overwrites last element)
+	for (int i = *pSize - 1; i > 0; i--)
+	{
+		array[i] = array[i - 1];
+	}
+	//go through the array and remove the first element
+	for (int i = 0; i < *pSize; i++)
+	{
+		array[i] = array[i + 1];
+	}
+
+	//reduce size of array
+	*pSize = *pSize - 1;
+
+	//store the values in the smaller array
+	for (int i = 0; i < *pSize; i++)
+	{
+		smallerArray[i] = array[i];
+	}
+
+	//delete the old(bigger) array
+	delete[] array;
+
+	//point to the smaller array
+	array = smallerArray;
+
+	return array;
+}
---
+++ standard-list-clone/pop.h
@@ -0,0 +1,7 @@
+#ifndef POP_H
+
+#define POP_H
+
+int* pop_back(int* array, int* pSize);
+
+#endif // !POP_H
